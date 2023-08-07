222d63c91cb58b4a066579e757012df9ab559caa
jobject <jobject@student.42.fr>
2023-08-07 19:36:58 +0200

    INIT

---
+++ level09/resources/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+        int i = 0;
+        while (argv[1][i]) {
+                printf("%c", argv[1][i] - i);
+                ++i;
+        }
+}
